85ce64f2c8858b6306923fd22aab52e67a5617c8
Suraj Gond <surajgond07@gmail.com>
2024-02-01 07:18:08 +0530

    signup page completed

--- app/src/main/java/com/surajinfotech/whatsapp/SignUpActivity.java
+++ app/src/main/java/com/surajinfotech/whatsapp/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.surajinfotech.whatsapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,12 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+//        action bar hide
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null)
+        {
+            actionBar.hide();
+        }
     }
 }
